ba84f47afa45ff0d1a8aa4693dd783221c858b3b
y9828 <y9828@naver.com>
2026-02-20 13:57:21 +0900

    [작업완료] security와 DB연동 완료

--- springBootSecurityDB/src/main/java/com/zeus/controller/NoticeController.java
+++ springBootSecurityDB/src/main/java/com/zeus/controller/NoticeController.java
@@ -1,5 +1,6 @@
 package com.zeus.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,8 @@ public class NoticeController {
 		
 		
 		} 
-		 
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")	 
 	@GetMapping("/register") 
 		public void registerForm() { 
 		log.info("notice registerForm : 로그인한 관리자만 접근 가능"); 
